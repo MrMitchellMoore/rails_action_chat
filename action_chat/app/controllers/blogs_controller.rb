c369a8462b92e721a43ca190256d5ee857c849f9
Mitchell Moore <mmoorejr24@gmail.com>
2019-12-10 18:10:18 -0800

    added flash and error messages

--- action_chat/app/controllers/blogs_controller.rb
+++ action_chat/app/controllers/blogs_controller.rb
@@ -1,13 +1,52 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
   def index
+  	@blogs = Blog.all
   end
 
   def show
   end
 
   def new
+  	@blog = Blog.new
+  end
+  
+  def create
+  	@blog = Blog.create(blog_params)
+  	if @blog.save
+  		flash[:success] = "Post is created."
+  		redirect_to @blog
+  	else
+  		flash.now[:error] = "Post was not created something went wrong."
+  		render :new
+  	end
+  end
+
+  def update
+  	if @blog.update(blog_params)
+  		flash[:success] = "Post updated."
+  		redirect_to @blogs	
+  	else
+  		flash.now[:error] = "Post not updated something went wrong."
+  		redirect_to @blogs 
+  	end	
   end
 
   def edit
+  end
+
+  def destroy
+  	@blog.destroy
+  	flash[:success] = "Post is deleted"
+  end
+
+  private
+
+  def set_blog
+  	@blog = Blog.find(params[:id])
+  end
+
+  def blog_params
+  	params.require(:blog).permit(:title, :content)
   end
 end
--- action_chat/app/helpers/application_helper.rb
+++ action_chat/app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+	# custom flash messages
+	def alert_class(flash_type)
+	    case flash_type.to_sym
+	    	when :notice
+	      	"alert-success"
+	    	when :alert
+	      	"alert-warning"
+	    	when :error
+	      	"alert-danger"
+	    end
+	end
 end
