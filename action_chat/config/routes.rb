e34825a3144df49f6779f74a25ce502793af69c2
Mitchell Moore <mmoorejr24@gmail.com>
2019-12-10 17:10:09 -0800

    created blog model and controller

--- action_chat/config/routes.rb
+++ action_chat/config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :blogs, only: [:index, :show, :new, :edit]
   resources :messages, only: [:index,:new, :create]
   root to: 'messages#index'
 end
